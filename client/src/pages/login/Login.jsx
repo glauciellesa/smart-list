4a4d3676645ebc108462ebf0f9526770e04bb20d
Glaucielle Sá <glauciellesa@gmail.com>
2023-08-25 15:44:13 +0200

    add json data

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-
+/* 
+https://github.com/glauciellesa.png
+https://github.com/CamilaFAssuncao.png
+https://github.com/leilaZ1111.png
+<img
+alt="Perfil image "
+src={`https://github.com/${props.imgProfile}.png`}
+/> */
 const Login = () => {
   return <div>Login</div>;
 };
