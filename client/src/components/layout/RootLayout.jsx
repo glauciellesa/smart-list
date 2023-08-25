cf9947e268c7889f8b5492f9b9599420228092e4
Glaucielle Sá <glauciellesa@gmail.com>
2023-08-25 13:11:38 +0200

    fix footer import

--- client/src/components/layout/RootLayout.jsx
+++ client/src/components/layout/RootLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { styled } from "styled-components";
 /* import Navbar from "../navbar"; */
-import Footer from "../footer";
+import Footer from "/../footer";
 
 const RootLayout = () => {
   return (
