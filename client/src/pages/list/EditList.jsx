12e570491ae67d4a4619982ac0bc18ec6513af0f
Camila Fernandes Assunção <129509305+CamilaFAssuncao@users.noreply.github.com>
2023-08-28 16:43:53 +0200

    importing items from json

--- client/src/pages/list/EditList.jsx
+++ client/src/pages/list/EditList.jsx
@@ -1,60 +1,62 @@
 import React, { useState } from 'react';
 import ListForm from './ListForm';
 import List from './List';
+import items from '../../../data/items.json';
+
 
 const EditList = () => {
-  const initialCategories = [
-    'fruits',
-    'vegetables',
-    'dairy',
-    'cereals',
-    'meat',
-    'cleaning',
-    'beauty',
-    'drinks'
-  ];
-
-  const [items, setItems] = useState([]);
-
-  const addItem = (text, category) => {
-    const existingItem = items.find(
-      (item) => item.text === text && item.category === category
-    );
-
-    if (existingItem) {
-      const updatedItems = items.map((item) =>
-        item.id === existingItem.id
-          ? { ...item, quantity: item.quantity + 1 }
-          : item
+    const initialCategories = [
+      'fruits',
+      'vegetables',
+      'dairy',
+      'cereals',
+      'meat',
+      'cleaning',
+      'beauty',
+      'drinks'
+    ];
+  
+    const [listItems, setListItems] = useState(items);
+  
+    const addItem = (text, category) => {
+      const existingItem = listItems.find(
+        (item) => item.text === text && item.category === category
       );
-      setItems(updatedItems);
-    } else {
-      const newItem = {
-        id: Math.floor(Math.random() * 10000),
-        text,
-        category,
-        quantity: 1,
-      };
-      setItems([...items, newItem]);
-    }
-  };
-
-  const removeItem = (id) => {
-    const newItems = items.filter((item) => item.id !== id);
-    setItems(newItems);
-  };
-
-  return (
-    <div>
-      <h1>Smart List App</h1>
-      <ListForm addItem={addItem} categories={initialCategories} />
-      <div className="create-list">
-        {items.map((item) => (
-          <List key={item.id} item={item} removeItem={removeItem} />
-        ))}
+  
+      if (existingItem) {
+        const updatedItems = listItems.map((item) =>
+          item.id === existingItem.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+        setListItems(updatedItems);
+      } else {
+        const newItem = {
+          id: Math.floor(Math.random() * 10000),
+          text,
+          category,
+          quantity: 1,
+        };
+        setListItems([...listItems, newItem]);
+      }
+    };
+  
+    const removeItem = (id) => {
+      const newItems = listItems.filter((item) => item.id !== id);
+      setListItems(newItems);
+    };
+  
+    return (
+      <div>
+        <h1>Smart List App</h1>
+        <ListForm addItem={addItem} categories={initialCategories} />
+        <div className="create-list">
+          {listItems.map((item) => (
+            <List key={item.id} item={item} removeItem={removeItem} />
+          ))}
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default EditList;
+    );
+  };
+  
+  export default EditList;
