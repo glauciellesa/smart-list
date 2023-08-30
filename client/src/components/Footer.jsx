7f6fd70e2f356091bf49f7316bc842ac6f47befd
leila <leila.zitouni55@gmail.com>
2023-08-30 11:34:36 +0200

    fix footer import

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import { styled } from 'styled-components';
 import { useState } from 'react';
-import { styled } from 'styled-components';
 
 const Footer = () => {
   const [test, setTest] = useState();
