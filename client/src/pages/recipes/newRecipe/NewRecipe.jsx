4262ae6b4f6f47ccd56e577671cad3a52f5e6b11
leila <leila.zitouni55@gmail.com>
2023-09-05 11:49:48 +0200

    progress on the new-recipe-page

--- client/src/pages/recipes/newRecipe/NewRecipe.jsx
+++ client/src/pages/recipes/newRecipe/NewRecipe.jsx
@@ -1,11 +1,71 @@
-import React from "react";
+import React from 'react';
+import { styled } from 'styled-components';
+
+const initialState = {
+  photo: '',
+  name: '',
+  timeToPrepare: '',
+  ingredients: [''],
+  instructions: '',
+};
 
 const NewRecipe = () => {
+  const [form, setForm] = useState(initialState);
+  const { addRecipe, error, isLoading } = useNewRecipe();
+
+  const handleChange = (event) => {
+    setForm({
+      ...form,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  const handleRecipe = async (e) => {
+    e.preventDefault();
+    await register(form);
+
+    setForm(initialState);
+  };
+
   return (
-    <div>
-      <h2>New Recipe</h2>
-    </div>
+    <StyleNewRecipe>
+      <form>
+        <div className="addAndSave">
+          <h1>New Recipe</h1>
+          <button onClick={handleRecipe} disabled={isLoading}>
+            Save
+          </button>
+        </div>
+        <label name="title">Title</label>
+        <input
+          type="text"
+          id="title"
+          placeholder="Give your recipe a name"
+          onChange={handleChange}
+          value={form.name}
+          required
+        />
+        <label name="timeToPrepare">Time to prepare</label>
+        <input
+          type="text"
+          id="timeToPrepare"
+          placeholder="How long does it take to prepare?"
+          onChange={handleChange}
+          value={form.timeToPrepare}
+          required
+        />
+      </form>
+    </StyleNewRecipe>
   );
 };
 
 export default NewRecipe;
+
+const StyleNewRecipe = styled.div`
+  form {
+    display: flex;
+    text-align: center;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
